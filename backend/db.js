a9fd2d13ba2c80e79aabcc3bcbbea9c1199ff219
Mahesh Sharma <maheshkaushik04611@gmail.com>
2023-08-08 10:39:02 +0530

    hosting

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,8 @@
+const { urlencoded } = require('express');
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://127.0.0.1:27017/inotebook?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0';
+const username = encodeURIComponent("maheshsvm");
+const password = encodeURIComponent("databaseHosting@123");
+const mongoURI = `mongodb+srv://${username}:${password}@cluster0.b142k1q.mongodb.net/inotebook?retryWrites=true&w=majority`;
 
 const connectToMongodb =  async()=>{
     await mongoose.connect(mongoURI);
